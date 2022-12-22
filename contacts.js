f789ce0d1d01b2037be85f7baacc8abbfc60ba81
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-12-23 00:52:20 +0100

    add contscts functions

---
+++ contacts.js
@@ -0,0 +1,36 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.resolve(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const db = JSON.parse(data);
+  return db;
+}
+
+async function getContactById(contactId) {
+  const db = await listContacts();
+  const findContact = db.find((contact) => contact.id === contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(findContact, null, 4));
+}
+
+async function removeContact(contactId) {
+  const db = await listContacts();
+  const updatedDb = db.filter((contact) => contact.id === contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(updatedDb, null, 4));
+}
+
+async function addContact(name, email, phone) {
+  const contact = { name, email, phone };
+  const db = await listContacts();
+  db.push(contact);
+  await fs.writeFile(contactsPath, JSON.stringify(db, null, 4));
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("/contacts.js");
+// const { program } = require("commander");
+
+async function invokeAction({ action, name, email, phone, contactId }) {
+  switch (action) {
+    case "list":
+      console.log("invoke list");
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+    case "get":
+      console.log("invoke get", contactId);
+      await getContactById(contactId);
+      break;
+    case "remove":
+      console.log("invoke remove", contactId);
+      await removeContact(contactId);
+      break;
+    case "add":
+      console.log("invoke add", name, email, phone);
+      await addContact(name, email, phone);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction({ action: "list" });
